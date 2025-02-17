de3f94c5a6635c11c7be37900211e99571e0a3c2
osamikoyo <v.pylayev@mail.ru>
2025-02-17 06:13:05 +0500

    add tokener

--- internal/indexer/indexer.go
+++ internal/indexer/indexer.go
@@ -1,5 +1,28 @@
 package indexer
 
+import (
+	"strings"
+
+	"github.com/osamikoyo/Locki/internal/models"
+)
+
 type Indexer struct{
-	Index map[string][]string
+	Word string
+	Index map[string][]models.Document
+}
+
+func New(word string) *Indexer {
+	return &Indexer{
+		Word: word,
+		Index: make(map[string][]models.Document),
+	}
+}
+
+func removeStopWords(data string) string {
+	replacer := strings.NewReplacer("a", "", "and", "", "the", "", "in", "", "on", "", "but", "", "or", "", "if", "")
+	return replacer.Replace(data)
+}
+
+func (i *Indexer) RegisterDocument(doc models.Document) {
+	doc.Data = removeStopWords(doc.Data)
 }
